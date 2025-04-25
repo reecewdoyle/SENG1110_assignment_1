d6dc943b3347f3f92ab49d58ba2bcee95b4909fb
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2025-04-26 01:49:56 +1000

    feat: added getters and setters in Project class

--- Project.java
+++ Project.java
@@ -14,6 +14,8 @@ public class Project {
     
     // Getters and Setters
     
+    // projectName
+
     public String getProjectName() {
         return projectName;
     }
@@ -23,5 +25,56 @@ public class Project {
     }
 
     // complete all other methods from here.
+
+    // projectId
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    // projectType
+
+    public String getProjectType() {
+        return projectType;
+    }
+
+    public void setProjectType(String projectType) {
+        this.projectType = projectType;
+    }
+    
+    // Task 1
+
+    public Task getTask1() {
+        return task1;
+    }
+
+    public void setTask1(Task task1) {
+        this.task1 = task1;
+    }
+
+    // Task 2
+
+    public Task getTask2() {
+        return task2;
+    }
+
+    public void setTask2(Task task2) {
+        this.task2 = task2;
+    }
+
+    // Task 3
+
+    public Task getTask3() {
+        return task3;
+    }
+
+    public void setTask3(Task task3) {
+        this.task3 = task3;
+    }
+        
     
 }
